7b2f58e5aee29c2a43680cad77008be3bc1c0dbc
Yoan Haouzi <yhaouzi@meilleursagents.com>
2023-01-04 14:19:13 +0100

    refactor: Clean up code by removing useless instructions

--- pricemap-python/pricemap/console.py
+++ pricemap-python/pricemap/console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import click
-import psycopg2
 
 
 @click.group()
@@ -15,16 +14,8 @@ def import_listings():
     Import listings by browsing through the listing api
     """
     from pricemap.usecase.import_listing import ImportListing
-    from pricemap.app import app
-    from flask import g
-
-    with app.app_context():
-        if not hasattr(app, 'db'):
-            app.db = psycopg2.connect(**app.config['DATABASE'])
-        g.db = app.db
 
     ImportListing().import_all_listings()
-
 
 if __name__ == '__main__':
     cli()
