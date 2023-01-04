d3f238bbc578c6a5be384e1d9ee3d6fab84a0190
Yoan Haouzi <yhaouzi@meilleursagents.com>
2023-01-04 15:55:33 +0100

    refactor: split listing data extraction to smaller methods

--- pricemap-python/pricemap/entity/listing.py
+++ pricemap-python/pricemap/entity/listing.py
@@ -18,42 +18,62 @@ class Listing:
         """
         # TODO : Find a better way to avoid encoding issues form the outside
         title = data['title'].replace("\u00a0", " ").replace("\u00e8", "è").replace("\u00b2", "²")
-        rooms_and_area = Listing.__parse_title(title)
         return Listing(
             int(data['listing_id']),
             place_id,
-            rooms_and_area['rooms'],
-            rooms_and_area['area'],
-            Listing.__parse_price(data['price']),
+            Listing.__extract_rooms_from_string(title),
+            Listing.__extract_area_from_string(title),
+            Listing.__extract_price_from_string(data['price']),
             seen_at
         )
 
     @staticmethod
-    def __parse_title(title: str) -> dict:
+    def __extract_rooms_from_string(title: str) -> int:
         """
-        Parse title string to retrieve rooms and surface_area
-        Could raise a ValueError when the title isn't correctly formed (for instance, missing surface_area information)
+        Parse title string to retrieve number of rooms
+        Set default value to 0 if the title does not match the regex
+        If the title contains 'Studio', that means 1 room
+        If the title contains 'Appartement', we extract the number of room just after
         examples :
-            - Appartement 2 pièces - 29 m² -> {'room': 2, 'area': 29}
-            - Studio - 6 m² -> {'room': 1, 'area': 6}
+            - Appartement 2 pièces - 29 m² -> 2
+            - Studio - 6 m² -> 1
+            - 12 m2 -> 0
+            - Appartement - 29m2 -> 0
         """
-        regex = re.compile(r'(?P<type>Appartement|Studio)( (?P<rooms>\d+) pièces)? - (?P<area>\d+) m²')
+        rooms = 0
+
+        regex = re.compile(r'(?P<type>Appartement|Studio)( (?P<rooms>\d+) pièces)?')
         matches = regex.search(title)
-        if matches is None:
-            raise ValueError('Unable to parse title for this listing - "%s"' % title)
-
-        if matches.group('type') == 'Studio':
-            rooms = 1
-        else:
-            rooms = int(matches.group('rooms'))
-
-        return {
-            'rooms': rooms,
-            'area': int(matches.group('area'))
-        }
+        if matches:
+            if matches.group('type') == 'Studio':
+                rooms = 1
+            else:
+                rooms = int(matches.group('rooms')) if matches.group('rooms') is not None else 0
+        return rooms
 
     @staticmethod
-    def __parse_price(price_str: str) -> int:
+    def __extract_area_from_string(title: str) -> int:
+        """
+        Parse title string to retrieve area
+        Set default value to 0 if the title does not match the regex
+
+        examples :
+            - Appartement 2 pièces - 29 m² -> 29
+            - Studio - 6 m² -> 6
+            - Appartement 3 pièces -> 0
+        """
+        area = 0
+
+        regex = re.compile(r'(?P<area>\d+) m²')
+        matches = regex.search(title)
+
+        if matches:
+            area = int(matches.group('area'))
+
+        return area
+
+    @staticmethod
+    def __extract_price_from_string(price_str: str) -> int:
         try:
             price = int("".join([s for s in price_str if s.isdigit()]))
         except:
