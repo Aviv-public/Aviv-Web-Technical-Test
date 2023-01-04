0e2020fc790eb4290732b26cbb1758006fc7b3e3
Yoan Haouzi <yhaouzi@meilleursagents.com>
2023-01-04 15:54:57 +0100

    refactor: Split import method to smaller methods
    - Add logger

--- pricemap-python/pricemap/__init__.py
+++ pricemap-python/pricemap/__init__.py
@@ -1,10 +1,20 @@
 """Handles database sessions and connections."""
+import logging
+import sys
+
 import psycopg2
-from psycopg2 import pool, extras
 import settings
+from psycopg2 import pool, extras
 
 db_pool = psycopg2.pool.ThreadedConnectionPool(1,
                                                20,
                                                **settings.DATABASE,
                                                cursor_factory=psycopg2.extras.DictCursor
                                                )
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
--- pricemap-python/pricemap/usecase/import_listing.py
+++ pricemap-python/pricemap/usecase/import_listing.py
@@ -1,3 +1,5 @@
+from pricemap import logger
+
 import requests
 from datetime import datetime
 
@@ -17,17 +19,27 @@ class ImportListing:
         Import listings for all places
         """
         for geom in GeoPlaceFinder.retrieve_all_places():
-            p = 0
-            while True:
-                p += 1
-                url = "http://listingapi:5000/listings/" + str(geom) + "?page=" + str(p)
-                d = requests.get(url)
+            logger.debug('Import listings for placeId {place_id}'.format(place_id = geom))
+            self.__import_place(geom)
 
-                # Break when finished
-                if d.status_code == 416:
-                    break
+    def __import_place(self, place_id: int):
+        """
+        Import all listings for a given place_id
+        That method will call an external API to retrieve data while the API returns a 200 status code,
+        then persist Listing entity into a dedicated table.
+        """
+        current_page = 1
 
-                for item in d.json():
-                    listing = Listing.from_data(item, geom, datetime.now())
+        while True:
+            response = requests.get(self.api_url % place_id, {'page': current_page})
+            if response.status_code != 200:
+                break
 
-                    self.listing_repository.insert(listing)
+            logger.info('Import listings for placeId {place_id} - page {page}'.format(place_id = place_id, page = current_page))
+
+            for item in response.json():
+                listing = Listing.from_data(item, place_id, datetime.now())
+                logger.debug('Prepare listing persistance {listing_id}'.format(listing_id = listing.id))
+
+                self.listing_repository.upsert(listing)
+            current_page += 1
