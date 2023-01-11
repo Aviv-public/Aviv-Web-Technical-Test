0ad33eab304c77bc41ecb599ec84eff44972f032
Elena <ebattisti@meilleursagents.com>
2023-01-11 17:25:07 +0100

    clean css

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ const Header = () => {
   return (
     <header className="header-box">
       <img className="logo" src="/assets/logo-aviv.svg" alt="logo Aviv" />
+      <div className="counter">23</div>
     </header>
   );
 };
--- front/src/containers/Listings/Listings.tsx
+++ front/src/containers/Listings/Listings.tsx
@@ -1,7 +1,9 @@
 const Listings = () => {
   return (
     <div className="container">
-      <h1>Welcome to the listing page</h1>
+      <h1 className="title">Welcome to the listing page</h1>
+      <div className="card" />
+      <div className="card card-selected" />
     </div>
   );
 };
