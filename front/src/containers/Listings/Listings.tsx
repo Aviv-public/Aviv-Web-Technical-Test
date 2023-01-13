c5b2d5367a53ca6b5cc17168ed903b8ca893bf03
emily <eblin@meilleursagents.com>
2023-01-13 14:56:58 +0100

    front : css for listing item

--- front/src/containers/Listings/Listings.tsx
+++ front/src/containers/Listings/Listings.tsx
@@ -2,7 +2,20 @@ const Listings = () => {
   return (
     <div className="container">
       <h1 className="title">Welcome to the listing page</h1>
-      <div className="card" />
+      <div className="card">
+        <input className="input" type="checkbox" />
+        <div>
+          <div className="top-block">
+            <h2 className="price">295 000 €</h2>
+            <span className="read">✓ Vu à 17h45</span>
+          </div>
+
+          <p className="place">T1 • Paris 17e</p>
+          <p className="reference"> ref 1969719989</p>
+          <span className="see-more">Voir l'annonce →</span>
+        </div>
+      </div>
+
       <div className="card card-selected" />
     </div>
   );
