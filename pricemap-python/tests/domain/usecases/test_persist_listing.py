d1a943328689fc48559cb9def6ab277ed085480c
Yoan Haouzi <yhaouzi@meilleursagents.com>
2023-01-16 11:44:54 +0100

    test: Add first test for Persist Usecase

---
+++ pricemap-python/tests/domain/usecases/test_persist_listing.py
@@ -0,0 +1,35 @@
+from logging import Logger
+from unittest.mock import Mock
+
+from pricemap.domain.ports import ListingRepository
+from pricemap.domain.usecases.persist_listing import PersistListing
+
+
+class TestPersistListing:
+
+    persist_listing_usecase: PersistListing
+
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.persist_listing_usecase = PersistListing(
+            Mock(ListingRepository), Mock(Logger)
+        )
+
+    def test_persist_listing(self) -> None:
+        input_data = {
+            "id": 3030030,
+            "name": "string",
+            "street_address": "48, boulevard des capucins",
+            "postal_code": "10294",
+            "city": "Paris",
+            "country_iso_2": "FR",
+            "description": "A description here",
+            "building_type": "HOUSE",
+            "price_eur": 340000,
+            "surface_area_m2": 43,
+            "rooms_count": 2,
+            "bedrooms_count": 1,
+            "contact_phone_number": "string",
+        }
+
+        self.persist_listing_usecase.do(input_data)
