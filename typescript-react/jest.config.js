294f0756ae5bfee07db7c641bda4e30e82d6cfee
newblues <asauvage@meilleursagents.com>
2023-03-03 15:03:44 +0100

    chore(ts-react): fix alias in jest config

--- typescript-react/jest.config.js
+++ typescript-react/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@containers/(.*)$': '<rootDir>/src/containers/$1',
   },
 };
