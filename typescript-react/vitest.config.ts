2b31566640f8e2cba7ab74c0d4957974c3184620
Kevin Nunes <kevin.nvk.nunes@gmail.com>
2023-08-18 16:37:26 +0200

    chore(deps): replace jest by vitest

--- typescript-react/vitest.config.ts
+++ typescript-react/vitest.config.ts
@@ -1,8 +1,31 @@
+import path from 'path';
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './test.setup.ts',
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src/'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@containers',
+        replacement: path.resolve(__dirname, 'src/containers'),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, 'src/styles'),
+      },
+    ],
   },
 });
