65610c6c5be47ae2618b2ff0861161d970e43ec4
Idar <idarnigatu@gmail.com>
2023-09-17 12:39:10 +0200

    feat: Update getListingPrices

--- typescript-serverless/src/functions/price/handler.ts
+++ typescript-serverless/src/functions/price/handler.ts
@@ -1,13 +1,25 @@
 import { functionHandler } from "@/libs/function";
 import { Price } from "@/types.generated";
+import { EntityNotFound, NotFound } from "@/libs/errors";
+import { getPriceRepository } from "@/repositories/prices";
 
-export const getListingPrices = functionHandler<Price[]>(async () => {
-  // Replace this with your implementation.
-  return {
-    statusCode: 200,
-    response: [
-      { price_eur: 100000, created_date: "2023-01-12T09:23:36Z" },
-      { price_eur: 150000, created_date: "2023-01-17T08:17:32Z" },
-    ],
-  };
-});
+export const getListingPrices = functionHandler<Price[]>(
+  async (event, context) => {
+    try {
+      const prices = await getPriceRepository(context.postgres).getPrices(
+        parseInt(event.pathParameters.id)
+      );
+
+      return {
+        statusCode: 200,
+        response: prices,
+      };
+    } catch (e) {
+      if (e instanceof EntityNotFound) {
+        throw new NotFound(e.message);
+      }
+
+      throw e;
+    }
+  }
+);
